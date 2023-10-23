0312312381b720923ca5129ec680a8b12d9d8d81
cafalchio <mcafalchio@gmail.com>
2023-10-23 11:32:24 +0100

    added cors

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import os
 from fastapi import FastAPI
 import json
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+origins = [
+    "https://diegocardenast.github.io/CodeBusters/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
 
 scores_file = "scores.json"
 
