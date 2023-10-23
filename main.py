ee1a46ebd42140a20fd9d1da823d0662a010dc85
cafalchio <mcafalchio@gmail.com>
2023-10-23 11:54:48 +0100

    removed coors

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 import os
 from fastapi import FastAPI
 import json
-from fastapi.middleware.cors import CORSMiddleware
+
 app = FastAPI()
 
-origins = [
-    # "https://diegocardenast.github.io/CodeBusters/",
-    # "https://diegocardenast.github.io/CodeBusters/scores",
-    "*"
-]
+# Comment out or remove the CORSMiddleware setup
+# origins = [
+#     "*"
+# ]
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],
-    allow_headers=["*"],
-)
-
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["GET", "POST", "PUT", "DELETE"],
+#     allow_headers=["*"],
+# )
 
 scores_file = "scores.json"
 
